e784c2829eb72d844b045bd141145a51d45a3bfe
Colin Rennie <colinmatthew@gmail.com>
2026-01-13 16:53:42 -0500

    Vulkan initial integration

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,7 @@
 #include <Application.hpp>
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include <vector>
+#include <stdexcept>
 
 void Application::run()
 {
@@ -18,6 +18,49 @@ void Application::initVulkan()
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+    createVkInstance();
+}
+
+void Application::createVkInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char *> requiredExtensions;
+
+    for (uint32_t i = 0; i < glfwExtensionCount; i++)
+    {
+        requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+    createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &vkInstance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create instance!");
+    }
 }
 
 void Application::mainLoop()
@@ -30,6 +73,8 @@ void Application::mainLoop()
 
 void Application::cleanup()
 {
+    vkDestroyInstance(vkInstance, nullptr);
+
     glfwDestroyWindow(window);
 
     glfwTerminate();
--- src/Application.hpp
+++ src/Application.hpp
@@ -1,12 +1,10 @@
 #include <cstdint>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
-
-namespace
-{
-    struct GLFWwindow;
-}
 
 class Application
 {
@@ -18,5 +16,8 @@ private:
     void mainLoop();
     void cleanup();
 
+    void createVkInstance();
+
     GLFWwindow *window;
+    VkInstance vkInstance;
 };
